73896187a0a7a2cf483ef51b3d78dff7f97e43bc
jose <a01570006@itesm.mx>
2020-05-15 23:36:00 -0500

    Assingment 2 coursera

---
+++ cachematrix.R
@@ -0,0 +1,18 @@
+getwd()
+## This script contain two functions that creates a special matrix with the makeCacheMatrix function 
+## then cacheSolve inverse the matrix. The interesting thing is that this functions saves the answers
+## in the cache, in case the same matrix is asked later.
+
+## This function creates the special matrix, which is a list, that saves in the cache the inverse matrix.
+
+makeCacheMatrix <- function(x = matrix()) {
+
+}
+
+
+## This function solves the special matrix from the makeCacheMatrix function, and checks if the answer is in the 
+## cache, if so, it retrieves a message and the solution.
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
